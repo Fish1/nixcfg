30b7cd4600254c0cb562d8ca3978ffc18a311610
Jacob Enders <jacobenders1@gmail.com>
2026-08-25 22:52:12 -0400

    update

--- hosts/jacob-laptop/configuration.nix
+++ hosts/jacob-laptop/configuration.nix
@@ -19,6 +19,8 @@
   services.desktopManager.plasma6.enable = true;
 
   virtualisation.docker.enable = true;
+  virtualisation.libvirtd.enable = true;
+  programs.virt-manager.enable = true;
 
   nix.gc = {
     automatic = true;
